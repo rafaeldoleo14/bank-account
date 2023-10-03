d75f26bff4072be8ef5c00a1a77a819e1d51399f
Rafael Francisco <rafaeldeoleo14@gmail.com>
2023-10-03 11:32:16 -0400

    feat: added initial route config

--- src/BankApp.tsx
+++ src/BankApp.tsx
@@ -1,10 +1,13 @@
 import { FC } from "react"
 import { Layout } from "./bank/layout/Layout"
+import { AppRouter } from "./bank/routes/AppRouter"
 
 export const BankApp: FC = () => {
   return (
     <Layout>
-      <h1></h1>
+
+      <AppRouter/>
+      
     </Layout>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
 import { BankApp } from './BankApp'
+import {BrowserRouter} from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BankApp/>
+    <BrowserRouter>
+      <BankApp/>
+    </BrowserRouter>
   </React.StrictMode>,
 )
