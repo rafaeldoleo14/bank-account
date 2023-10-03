86d8b4cf5aec8dbb4e3b4f2ddab9ac6ca39255ca
Rafael Francisco <rafaeldeoleo14@gmail.com>
2023-10-03 09:30:41 -0400

    feat: changes to initial structure

---
+++ src/BankApp.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react"
+
+
+export const BankApp: FC = () => {
+  return (
+    <div>BankApp</div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { BankApp } from './BankApp'
+import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BankApp/>
   </React.StrictMode>,
 )
