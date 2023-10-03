512064db392e86e12baddc02340f4385dcfa9802
Jeromy <jeromi.mercedesdiaz@gmail.com>
2023-10-03 14:54:06 -0400

    feat: added navbar components

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
 import { BankApp } from './BankApp'
+import { NavBar } from './bank/components/navbar/Navbar'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <NavBar />
     <BankApp/>
   </React.StrictMode>,
 )
