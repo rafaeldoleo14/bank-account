25753ff0daac798f7bfd44f35af2a94efa9d45aa
Rafael Francisco <rafaeldeoleo14@gmail.com>
2023-10-03 12:03:56 -0400

    feat: added link active hover

--- src/bank/components/dashboard/Dashboard.tsx
+++ src/bank/components/dashboard/Dashboard.tsx
@@ -1,14 +1,18 @@
 import { FC } from "react"
 import './Dasboard.css';
 import { dashboardOpcions } from "../../utils/getDashboardOpcions";
+import {NavLink} from 'react-router-dom'
 
 export const Dashboard: FC = () => {
+
   return (
     <div className="menu-container">
 
       {
-        dashboardOpcions.map(({text, icon})=> (
-          <p key={text} className="active"> {icon} {text}</p>
+        dashboardOpcions.map(({text, icon, path})=> (
+          <NavLink key={text} to={path}>
+            {icon} {text}
+          </NavLink>
         ))
       }
 
